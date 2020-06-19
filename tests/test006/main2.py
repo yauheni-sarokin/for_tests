caf03c61a0a840b5314b6a30d72dea38d8133a55
yauheni-sarokin <sarokin.yauheni@gmail.com>
2020-06-19 15:50:28 +0300

    -add abstract factory

---
+++ tests/test006/main2.py
@@ -0,0 +1,26 @@
+'''
+Getter setter OOP way
+Property
+'''
+
+
+class Property:
+    def __init__(self, var) -> None:
+        self.a = var
+
+    @property
+    def a(self):
+        return self.__a
+
+    @a.setter
+    def a(self, var):
+        if var > 0 and var % 2 == 0:
+            self.__a = var
+        else:
+            self.__a = 2
+
+
+if __name__ == "__main__":
+    obj = Property(123)
+
+    print(obj.a)
