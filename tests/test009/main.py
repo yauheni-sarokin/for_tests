73f229af24345bbc92611131028917c6f613d635
yauheni-sarokin <sarokin.yauheni@gmail.com>
2020-06-19 20:45:39 +0300

    -add study metaclass

--- tests/test009/main.py
+++ tests/test009/main.py
@@ -8,4 +8,7 @@ needs to have prior experience working with Python classes. Before we dive
 deeper into metaclasses, let's get a few concepts out of the way.
 
 https://www.datacamp.com/community/tutorials/python-metaclasses?utm_source=adwords_ppc&utm_campaignid=898687156&utm_adgroupid=48947256715&utm_device=c&utm_keyword=&utm_matchtype=b&utm_network=g&utm_adpostion=&utm_creative=332602034349&utm_targetid=aud-392016246653:dsa-429603003980&utm_loc_interest_ms=&utm_loc_physical_ms=9061558&gclid=CjwKCAjwxLH3BRApEiwAqX9arYBJ_EdSj8LVBfym6CfH6_c6Ce_62sO1QD9nJ9bKt7vxxwh6Y60hMRoCCWAQAvD_BwE
+
+this after
+https://codesachin.wordpress.com/2016/06/09/the-magic-behind-attribute-access-in-python/
 """
