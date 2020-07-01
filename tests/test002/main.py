1c36cbb64e411fc334f43b7fe69c58c38c70a09a
yauheni-sarokin <sarokin.yauheni@gmail.com>
2020-07-01 10:23:33 +0300

    -add remove idea folder

---
+++ tests/test002/main.py
@@ -0,0 +1,32 @@
+'''
+Abstract base classes
+'''
+#the infrastructure for defining Abstract Base Classes (ABCs).
+from abc import ABC, abstractmethod
+
+
+class AbstractClassExample(ABC):
+
+    def __init__(self, value):
+        self.value = value
+        super().__init__()
+
+    @abstractmethod
+    def do_something(self):
+        pass
+
+class DoAdd42(AbstractClassExample):
+
+    def do_something(self):
+        return self.value + 42
+
+class DoMu142(AbstractClassExample):
+
+    def do_something(self):
+        return self.value * 42
+
+x = DoAdd42(10)
+y = DoMu142(10)
+
+print(x.do_something())
+print(y.do_something())
---
+++ tests/test009/main.py
@@ -0,0 +1,27 @@
+"""
+what is metaclass
+
+A metaclass in Python is a class of a class that defines how a class behaves.
+A class is itself an instance of a metaclass. A class in Python defines how the
+instance of the class will behave. In order to understand metaclasses well, one
+needs to have prior experience working with Python classes. Before we dive
+deeper into metaclasses, let's get a few concepts out of the way.
+
+https://www.datacamp.com/community/tutorials/python-metaclasses?utm_source=adwords_ppc&utm_campaignid=898687156&utm_adgroupid=48947256715&utm_device=c&utm_keyword=&utm_matchtype=b&utm_network=g&utm_adpostion=&utm_creative=332602034349&utm_targetid=aud-392016246653:dsa-429603003980&utm_loc_interest_ms=&utm_loc_physical_ms=9061558&gclid=CjwKCAjwxLH3BRApEiwAqX9arYBJ_EdSj8LVBfym6CfH6_c6Ce_62sO1QD9nJ9bKt7vxxwh6Y60hMRoCCWAQAvD_BwE
+
+this after
+https://codesachin.wordpress.com/2016/06/09/the-magic-behind-attribute-access-in-python/
+"""
+
+class MyMeta(type):
+    pass
+
+class MyClass(metaclass=MyMeta):
+    pass
+
+class MySubclass(MyClass):
+    pass
+
+print(type(MyMeta))
+print(type(MyClass))
+print(type(MySubclass))
